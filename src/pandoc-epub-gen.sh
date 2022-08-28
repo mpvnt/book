6ee7c2327faaf084db4f89db71ee044e61532096
mpvnt <>
2022-08-28 08:31:10 -0500

    Changed script to generate table of contents rather than use summary page.

--- src/pandoc-epub-gen.sh
+++ src/pandoc-epub-gen.sh
@@ -1,6 +1,5 @@
-pandoc --metadata-file=meta.yml -o epub/the-rust-book.epub --css ../theme/2018-edition.css  -f markdown-tex_math_dollars \
+pandoc --metadata-file=meta.yml --toc -o epub/the-rust-book.epub --css ../theme/2018-edition.css  -f markdown-tex_math_dollars \
        title-page.md\
-       SUMMARY.md\
        foreword.md\
        ch*.md\
        appendix-*.md
